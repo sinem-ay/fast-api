f45d6667a32f8a9dc62956cdd650f33be321582d
sinem-ay <sinem.ayyildiz98@gmail.com>
2022-07-26 13:06:15 +0300

    connect database

---
+++ database.py
@@ -0,0 +1,10 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine("postgresql://postgres:sin23a@localhost/item_db", echo=True)
+
+Base = declarative_base()
+
+SessionLocal = sessionmaker(bind = engine)
+
