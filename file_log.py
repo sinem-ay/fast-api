76dd124765aa1db089a532928e18fc5dfb695f80
sinem-ay <sinem.ayyildiz98@gmail.com>
2022-08-09 13:43:12 +0300

    logger in uvicorn

---
+++ file_log.py
@@ -0,0 +1,50 @@
+import os
+import logging
+import sys
+import main
+
+from uvicorn import Config, Server
+from loguru import logger
+
+LOG_LEVEL = logging.getLevelName(os.environ.get("LOG_LEVEL", "DEBUG"))
+JSON_LOGS = True if os.environ.get("JSON_LOGS", "0") == "1" else False
+
+
+class InterceptHandler(logging.Handler):
+    def emit(self, record):
+        try:
+            level = logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
+
+        frame, depth = logging.currentframe(), 2
+        while frame.f_code.co_filename == logging.__file__:
+            frame = frame.f_back
+            depth += 1
+
+        logger.opt(depth=depth, exception=record.exc_info).log(level, record.getMessage())
+
+
+def setup_logging():
+    logging.root.handlers = [InterceptHandler()]
+    logging.root.setLevel(LOG_LEVEL)
+
+    for name in logging.root.manager.loggerDict.keys():
+        logging.getLogger(name).handlers = []
+        logging.getLogger(name).propagate = True
+
+    logger.configure(handlers=[{"sink": sys.stdout, "serialize": JSON_LOGS}])
+
+
+if __name__ == '__main__':
+    server = Server(
+        Config(
+            "main:app",
+            host="127.0.0.1",
+            log_level=LOG_LEVEL,
+        ),
+    )
+
+setup_logging()
+
+server.run()
