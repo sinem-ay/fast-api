9e32fff6e1ace10f3b61a557fe4b625705607e2d
sinem-ay <sinem.ayyildiz98@gmail.com>
2022-08-16 11:24:17 +0300

    new column added

--- create_database.py
+++ create_database.py
@@ -2,4 +2,4 @@ from database import Base, engine
 from models import Games
 
 # Creating database
-Base.metadata.create_all(engine)
+Base.metadata.create_all(engine, checkfirst=True)
--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from database import Base
+from database import Base, engine
 from sqlalchemy import String, Integer, Column, DateTime
 
 
@@ -6,12 +6,12 @@ from sqlalchemy import String, Integer, Column, DateTime
 class Games(Base):
     __tablename__ = 'games'
     id = Column(Integer, primary_key=True, unique=True, nullable=False)
-    game_name = Column(String, unique=True, nullable=False)
-    game_type = Column(String, nullable=False)
+    game_name = Column(String(40), unique=True, nullable=False)
+    game_type = Column(String(40), nullable=False)
     price = Column(Integer, nullable=False)
-    company = Column(Integer, nullable=False)
     country = Column(String, nullable=False)
     release_date = Column(DateTime, nullable=True)
+    autoload_with = engine
 
     def __repr__(self):
         return f"<Game name={self.game_name} price={self.price}>"
@@ -30,3 +30,14 @@ class PersonalRanking(Base):
     id = Column(Integer, primary_key=True, unique=True, nullable=False)
     game_name = Column(String, unique=True, nullable=False)
     rank = Column(Integer, nullable=False)
+
+
+def add_column(engine, table_name, column):
+    column_name = column.compile(dialect=engine.dialect)
+    column_type = column.type.compile(engine.dialect)
+    engine.execute('ALTER TABLE %s ADD COLUMN %s %s' % (table_name, column_name, column_type))
+
+
+table_name = 'games'
+column = Column('company', String(100), nullable=False)
+add_column(engine, table_name, column)
